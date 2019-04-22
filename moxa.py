d7b587354b6968a15bee8405c602fb03a4d7d857
Sean Liu <seanhf.liu@moxa.com>
2019-04-22 13:57:23 +0800

    test

--- moxa.py
+++ moxa.py
@@ -2,18 +2,18 @@ import numpy
 import scipy.signal
 import thinkdsp
 
-def filter_data_batch( batch_data, filter_type = 'gaussian', window_size = 11, std = 2 ):
+def filter_data_batch( batch_data, filter_type = 'gaussian', window_size = 11, std = 1 ):
     filtered_data = [ filter_data( data, filter_type = filter_type, window_size = window_size, std = std ) for data in batch_data ]
     filtered_data = numpy.array( filtered_data )
     return filtered_data
 
-def filter_data( data, filter_type = 'gaussian', window_size = 11, std = 2 ):
+def filter_data( data, filter_type = 'gaussian', window_size = 11, std = 1 ):
     gaussian = scipy.signal.gaussian( M = window_size, std = std )
     gaussian /= sum( gaussian )
     convolved = numpy.convolve( data, gaussian, mode = 'same' )
     return convolved
 
-def get_test_data( freq, n_offset = 10, random_seed = 42, shape_list = [ 'sawtooth', 'square', 'triangle' ], noise_ratio = 0 ):
+def get_test_data( freq, n_offset = 10, random_seed = 42, shape_list = [ 'sawtooth', 'square', 'triangle', 'cosine', 'chirp' ], noise_ratio = 0 ):
     numpy.random.seed( random_seed )
     offset_list = 2 * numpy.pi * numpy.random.uniform( size = n_offset )
     data_x, data_y = get_wave_data( freq, offset_list, duration = 0.1, framerate = 40000, shape_list = shape_list )
@@ -68,22 +68,22 @@ def get_train_data_v3( n_offset = 10, shape_list = [ 'sawtooth', 'square', 'tria
     data_y = numpy.expand_dims( data_y, axis = 1 )
     return data_x, data_y
 
-def get_wave_data( freq, offset_list, duration = 0.1, framerate = 40000, shape_list = [ 'sawtooth', 'square', 'triangle' ] ):
+def get_wave_data( freq, offset_list, duration = 0.1, framerate = 40000, shape_list = [ 'sawtooth', 'square', 'triangle', 'cosine' ] ):
     data_x, data_y = [], []
 
     for offset in offset_list:
         duration = 0.1
         framerate = 40000
 
-        # Sawtooth
-        if 'sawtooth' in shape_list:
-            data = get_sawtooth_wave( freq = freq, offset = offset, duration = duration, framerate = framerate )
-            data_x.append( data )
-            data_y.append( freq )
+        # # Sawtooth
+        # if 'sawtooth' in shape_list:
+        #     data = get_sawtooth_wave_data( freq = freq, offset = offset, duration = duration, framerate = framerate, noise_ratio = 0 )
+        #     data_x.append( data )
+        #     data_y.append( freq )
 
         # Square
         if 'square' in shape_list:
-            data = get_square_wave( freq = freq, offset = offset, duration = duration, framerate = framerate )
+            data = get_square_wave_data( freq = freq, offset = offset, duration = duration, framerate = framerate, noise_ratio = 0 )
             data_x.append( data )
             data_y.append( freq )
 
@@ -105,13 +105,26 @@ def get_wave_data( freq, offset_list, duration = 0.1, framerate = 40000, shape_l
             data_x.append( data )
             data_y.append( freq )
 
+        if 'chirp' in shape_list:
+            data = get_chirp_wave_data( freq = freq, offset = offset, duration = duration, framerate = framerate )
+            data_x.append( data )
+            data_y.append( freq )
+
     return data_x, data_y
 
-def get_sawtooth_wave( freq, offset, duration = 0.1, framerate = 40000 ):
+def get_sawtooth_wave_data( freq, offset, duration = 0.1, framerate = 40000, noise_ratio = 0 ):
     signal = thinkdsp.SawtoothSignal( freq = freq, amp = 1.0, offset = offset )
     wave = signal.make_wave( duration = duration, start = 0, framerate = framerate )
+    if noise_ratio != 0:
+        wave.ys += numpy.random.standard_normal( wave.ys.shape ) * noise_ratio
     return wave.ys
 
+def get_square_wave_data( freq, offset, duration = 0.1, framerate = 40000, noise_ratio = 0 ):
+    signal = thinkdsp.SquareSignal( freq = freq, amp = 1.0, offset = offset )
+    wave = signal.make_wave( duration = duration, start = 0, framerate = framerate )
+    if noise_ratio != 0:
+        wave.ys += numpy.random.standard_normal( wave.ys.shape ) * noise_ratio
+    return wave.ys
 def get_square_wave( freq, offset, duration = 0.1, framerate = 40000 ):
     signal = thinkdsp.SquareSignal( freq = freq, amp = 1.0, offset = offset )
     wave = signal.make_wave( duration = duration, start = 0, framerate = framerate )
@@ -134,4 +147,11 @@ def get_sawtooth_square_wave( freq, offset, duration = 0.1, framerate = 40000 ):
     signal = thinkdsp.SumSignal( *components )
     wave = signal.make_wave( duration = duration, start = 0, framerate = 40000 )
     wave.ys = wave.ys / len( components ) # normalization
+    return wave.ys
+
+def get_chirp_wave_data( freq, offset, duration = 0.1, framerate = 40000, noise_ratio = 0 ):
+    signal = thinkdsp.Chirp( start = freq * 0.5, end = 1.5, amp = 1.0 )
+    wave = signal.make_wave( duration = duration, start = 0, framerate = framerate )
+    if noise_ratio != 0:
+        wave.ys += numpy.random.standard_normal( wave.ys.shape ) * noise_ratio
     return wave.ys
